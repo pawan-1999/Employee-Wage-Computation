00569ca2aa828cf51bc979297d82068e77989028
pawan-1999 <guptapawan767@gmail.com>
2021-06-30 18:50:02 +0530

    to check whether the employee is present is absent

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeAttendence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
