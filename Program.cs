92bec118a3ae3455890fb41b3956d9186776b2e9
pawan-1999 <guptapawan767@gmail.com>
2021-06-30 19:04:33 +0530

    Employees which are part time and full time to compute there wages

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace UC_3PartTimeEmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constatnts
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_WAGE_PER_HOUR = 20;
+            //Variable
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_WAGE_PER_HOUR;
+            Console.WriteLine("Emp wage : "+empWage);
+        }
+    }
+}
+
